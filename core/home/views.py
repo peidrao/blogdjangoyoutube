f712721e1ae935b0eede4d2956100c667a077d06
Pedro Victor da Fonseca <peidrao>
2021-01-01 22:10:58 -0300

    paginaçao

--- core/home/views.py
+++ core/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 from post.models import Post, Category
@@ -14,3 +15,22 @@ def index(request):
         'category': category
     }
     return render(request, 'index.html', context)
+
+def blog(request):
+    post = Post.objects.all()
+    category = Category.objects.all()
+    post_latest = Post.objects.order_by('id')[:3]
+
+
+    paginator = Paginator(post, 4) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'category': category,
+        'post_latest': post_latest,
+    }
+    return render(request, 'pages/blog.html', context)
+    
+    
